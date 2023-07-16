ea617f1840455ed2f4a639cee5aebfd9550cacb9
Peter Muessig <peter.muessig@sap.com>
2023-07-16 19:13:33 +0200

    feat: add support for globalContext

--- src/index.js
+++ src/index.js
@@ -46,25 +46,33 @@ jsep.plugins.register(
 );
 
 /**
- * Injects the template string into the given tag function.
- * @param {Function} tagFn the template tag function (e.g. String.raw or html)
- * @param {string|HTMLTemplateElement} stringOrHTMLTemplate the template string or a reference to an HTMLTemplateElement
- * @returns {any} the output of the template tag function using the given template string
+ * Builds a template function out of the provided `string` or the innerHTML of the provided `HTMLTemplateElement`
+ * which is executed using the given template tag function with the provided `context` and optionally an additional
+ * `globalContext`.
+ *
+ * Usage example:
+ * ```js
+ * const ttFn = ttlit(String.raw, "Hello ${name}");
+ * ttFn({ name: "World" }); // returns "Hello World"
+ * ```
+ * @param {Function} tagFn template tag function (e.g. `String.raw` or `html`)
+ * @param {string|HTMLTemplateElement} stringOrHTMLTemplate template `string` or reference to a `HTMLTemplateElement`
+ * @returns {function(object, [object]): any} `function` executing the template tag function with a given context and optionally a globalContext and returning its result
  */
-export default function buildTemplate(tagFn, stringOrHTMLTemplate) {
+export default function buildTemplateFunction(tagFn, stringOrHTMLTemplate) {
 	let string = stringOrHTMLTemplate;
 	if (string instanceof HTMLTemplateElement) {
 		string = string.innerHTML.replaceAll("&gt;", ">").replaceAll("&lt;", "<").trim();
 	}
 	if (typeof string === "string") {
 		const info = parseTemplateLiteral(string);
-		return (ctx) => {
+		return (context, globalContext) => {
 			return tagFn(
 				info.strings,
 				...info.values.map((v) => {
 					try {
 						const ast = jsep(v);
-						const value = executeAST(ast, ctx);
+						const value = executeAST(ast, context, globalContext);
 						return value;
 					} catch (ex) {
 						// failed to parse the expression!
